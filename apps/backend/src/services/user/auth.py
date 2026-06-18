de1caf5af1951a2dd7fde402a5f2575469bab850
kirillaim <kirillaim.in.ua@gmail.com>
2026-06-18 07:22:06 +0000

    auuth srvice

--- apps/backend/src/services/user/auth.py
+++ apps/backend/src/services/user/auth.py
@@ -1,21 +1,34 @@
 
+from apps.backend.src.core.db.uow import UnitOfWork
 from core import utils
 from database.repositories.users import UserRepository
 
 
 class UserAuthService:
-    def __init__(self, user_repository: UserRepository):
-        self.user_repository: UserRepository = user_repository
+    def __init__(self, uow: UnitOfWork):
+        self.uow: UnitOfWork = uow
 
 
-    def authenticate_user(self, email: str, password: str) -> bool:
-        async with uow as uow:
-            user = self.user_repository.get_by_email(email)
+    async def authenticate_user(self, email: str, password: str) -> bool:
+        async with self.uow as uow:
+            user = await uow.users.get_by_email(email)
             
         if not user:
             return False
         return self.verify_password(password, user.pass_hash)
 
     def verify_password(self, password: str, pass_hash: str) -> bool:
-        # Implement password verification logic here (e.g., using bcrypt)
-        return password == pass_hash  # Placeholder for demonstration
+        
+        utils.validate_password(password, pass_hash.encode())
+        
+        return password == pass_hash
+    
+    async def register_user(self, email: str, password: str, username: str) -> bool:
+        async with self.uow as uow:
+            user = uow.users.get_by_email(email)
+            if user:
+                return False
+            
+            pass_hash = utils.hash_password(password)
+            new_user = uow.users.create_user(email=email, pass_hash=pass_hash)
+            return True
